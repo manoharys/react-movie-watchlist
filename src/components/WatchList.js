c04520b5cc3c702e6a9e5485b8346b4c49b2da7c
manohar <manumanohar1527@gmail.com>
2020-12-22 10:09:24 +0530

    initial setup

---
+++ src/components/WatchList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const WatchList = () => {
+    return (
+        <div>
+            WatchList
+        </div>
+    )
+}
---
+++ src/components/Watched.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Watched = () => {
+    return (
+        <div>
+            Watched
+        </div>
+    )
+}
