690c1dbf7666d612ac2e85b6d637d55371bb88b1
manohar <manumanohar1527@gmail.com>
2020-12-22 20:56:57 +0530

    Making API request

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,10 +1,50 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+//Api key
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+console.log(API_KEY);
 export const Add = () => {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
+  //Making a api request
+  useEffect(() => {
+    axios
+      .get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: API_KEY,
+          query: query,
+          language: "en-US",
+          page: 1,
+          include_adult: true,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setResults(res.data.results);
+   
+      })
+      .catch((err) => console.log(err));
+  }, [query]);
 
   return (
-    
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <div className="input-wrapper">
+            <input
+              type="text"
+              placeholder="Search for a movie..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </div>
+          {results.map((movie) => (
+            <li>{movie.id + movie.title}</li>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
