15c617d4992a9de6bc26c1b5a56b2569e46c4301
manohar <manumanohar1527@gmail.com>
2020-12-25 16:44:04 +0530

    added localStorage to the app

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,25 +1,37 @@
-import React, { useReducer } from 'react'
-import {reducer} from "./reducer";
+import React, { useReducer, useEffect } from "react";
+import { reducer } from "./reducer";
+
+const initialState = {
+  watchList: localStorage.getItem("watchList")
+    ? JSON.parse(localStorage.getItem("watchList"))
+    : [],
+  watched: [],
+};
 
 export const globalContext = React.createContext();
 
 export const ACTIONS = {
-    ADD_TO_WATCHLIST : "addToWatchList"
-}
+  ADD_TO_WATCHLIST: "addToWatchList",
+};
 
-export const GlobalContextProvider = (props)=>{
+export const GlobalContextProvider = (props) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
-   
-   const [state, dispatch] = React.useReducer(reducer, {watchList:[], watched:[]});
-   
-   const addMovie = (movie)=>{
-       dispatch({type: ACTIONS.ADD_TO_WATCHLIST, payLoad: movie});
-   }
-   
-   return (
-       <globalContext.Provider value={{watchList: state.watchList, watched: state.watched, addMovie}}>
-           {props.children}
-       </globalContext.Provider>
-   )
+  useEffect(() => {
+    localStorage.setItem("watchList", JSON.stringify(state.watchList));
+    localStorage.setItem("watched", JSON.stringify(state.watched));
+    console.log(JSON.parse(localStorage.getItem("watchList")));
+  }, [state]);
 
-}
+  const addMovie = (movie) => {
+    dispatch({ type: ACTIONS.ADD_TO_WATCHLIST, payLoad: movie });
+  };
+
+  return (
+    <globalContext.Provider
+      value={{ watchList: state.watchList, watched: state.watched, addMovie }}
+    >
+      {props.children}
+    </globalContext.Provider>
+  );
+};
